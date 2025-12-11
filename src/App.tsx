384aa34df85fc772f41011cc11dfa9d5248a4e3f
Uttam Deb <uttamdeb670@outlook.com>
2025-12-11 12:32:47 +0600

    Mode
    Mode

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
