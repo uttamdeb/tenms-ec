4e80689953684ef56edb3426d1893acecd21f688
Uttam Deb <uttamdeb670@outlook.com>
2026-05-04 11:44:56 +0600

    Chore
    Chore

--- src/lib/tenmsAuth.ts
+++ src/lib/tenmsAuth.ts
@@ -4,9 +4,9 @@ export const TENMS_CLIENT_ID = "tenms_97b90a8f9c72ba6c3dc777c37df5ea9d";
 
 const redirectUri =
   typeof window !== "undefined"
-    ? `${window.location.origin}${window.location.pathname}`
+    ? 'https://tenms-data-agent.lovable.app/'
     : undefined;
-    
+
 export const tenmsAuth = new TenMSAuth({
   clientId: TENMS_CLIENT_ID,
   redirectUri,
