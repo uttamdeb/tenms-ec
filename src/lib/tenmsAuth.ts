01f1bf6004a802e4fce164f4ae3d0361619a4d4d
Uttam Deb <uttamdeb670@outlook.com>
2026-05-04 11:25:32 +0600

    Chore
    Chore

--- src/lib/tenmsAuth.ts
+++ src/lib/tenmsAuth.ts
@@ -4,5 +4,5 @@ export const TENMS_CLIENT_ID = "tenms_97b90a8f9c72ba6c3dc777c37df5ea9d";
 
 export const tenmsAuth = new TenMSAuth({
   clientId: TENMS_CLIENT_ID,
-  storage: "localStorage",
+  // storage: "localStorage",
 });
